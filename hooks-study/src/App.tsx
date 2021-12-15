25f4e3a345e7e7d035b4e246373ba976279e9c83
notiger390 <notiger390@gmail.com>
2021-12-15 12:17:37 +0900

    複数のstate管理

--- hooks-study/src/App.tsx
+++ hooks-study/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './componets/counter';
+import InputForm from './componets/InputForm';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <InputForm></InputForm>
     </div>
   );
 }
